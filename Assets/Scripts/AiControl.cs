2d5695b1ec8b176753750dd929ddac2fa8e91a5d
caveiramica14 <a63586@alunos.ipb.pt>
2026-09-21 10:24:39 +0100

    Moving As One

---
+++ Assets/Scripts/AiControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiControl : MonoBehaviour 
+{
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    void Start() 
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+
+
+    void Update() 
+    {
+
+    }
+}
